5da8dc65e61a61c1805e12b25c3b90542ba8700a
canantu <tunc.canan84@gmail.com>
2024-03-06 14:36:14 +0100

    [u] Abstraction

--- Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-04-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -4,6 +4,7 @@ package com.cydeo.service;
 import com.cydeo.model.Comment;
 import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,7 +12,8 @@ public class CommentService {
 
     private final CommentRepository commentRepository; // dependency classes are generally defined as final
     private final CommentNotificationProxy commentNotificationProxy;
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
+    //commentPushNotificationProxy or emailCommentNotificationProxy -> bean names put by Spring
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
