da21f66c4dd1893b9ad19bc5064ba1521720d0d2
canantu <tunc.canan84@gmail.com>
2024-03-10 11:06:06 +0100

    [+] Spring Boot Demo

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,25 @@
+package com.cydeo.service;
+
+
+import com.cydeo.model.Comment;
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository; // dependency classes are generally defined as final
+    private final CommentNotificationProxy commentNotificationProxy;
+    //commentPushNotificationProxy or emailCommentNotificationProxy -> bean names put by Spring
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+}
