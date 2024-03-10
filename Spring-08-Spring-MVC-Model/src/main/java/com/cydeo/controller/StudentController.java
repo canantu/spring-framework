2ddc11b252a095ec2e660c3d3500722bc43e3cb5
canantu <tunc.canan84@gmail.com>
2024-03-10 23:25:54 +0100

    [u] Spring MVC Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -4,13 +4,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model){
+
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
 
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        Integer id = new Random().nextInt();
+        model.addAttribute("studentId", id);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(6);
+        numbers.add(47);
+        numbers.add(45);
+
+        model.addAttribute("numbers", numbers);
         return "student/welcome";
     }
 }
