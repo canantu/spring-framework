4e56139de6b322b144f4a64f74be187004469b8b
canantu <tunc.canan84@gmail.com>
2024-04-27 12:04:40 +0200

    [+] Spring ORM Configuration and Create Table

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,15 +1,34 @@
 package com.cydeo.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.cydeo.enums.Gender;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
+@Table(name = "students")
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "studentFirtName", length = 100)
     private String firstName;
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
 
+    @Transient
+    private String city;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate birthday;
+    @Column(columnDefinition = "TIME")
+    private LocalTime birthTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
 
 }
