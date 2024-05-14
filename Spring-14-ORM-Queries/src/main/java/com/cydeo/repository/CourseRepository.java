f74412d137be2107306387b0a37fd42da7535513
canantu <tunc.canan84@gmail.com>
2024-05-14 22:50:27 +0200

    [+] Spring ORM - Queries

--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.cydeo.repository;
 
 import com.cydeo.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,6 +25,9 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     // find all courses starts with provided course name
     List<Course> findByNameStartsWith(String name);
+
+    @Query("select c from Course c where c.category = :category and c.rating > :rating")
+    List<Course> findAllByCategoryAndRatingGreaterThan(@Param("category") String category, @Param("rating") int rating);
 
 
 }
--- Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
+++ Spring-14-ORM-Queries/src/main/java/com/cydeo/repository/DepartmentRepository.java
@@ -2,6 +2,7 @@ package com.cydeo.repository;
 
 import com.cydeo.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,5 +18,13 @@ public interface DepartmentRepository extends JpaRepository<Department, String> 
 
     // display top 3 departments with division name includes "Hea" without duplicates
     List<Department> findDistinctTop3ByDivisionContains(String pattern);
+
+    @Query("select d from Department  d where d.division in ?1")
+    List<Department> getDepartmentDivision(List<String> divisionList);
+
+    List<Department> retrieveDepartmentByDivision(String division);
+
+    @Query(nativeQuery = true)
+    List<Department> retrieveDepartmentByDivisionContain(String pattern);
 
 }
