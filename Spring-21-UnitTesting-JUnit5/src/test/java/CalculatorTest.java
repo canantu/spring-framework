a09a5cb1667cb8869ab37e4e2bde5d336293d7aa
canantu <tunc.canan84@gmail.com>
2024-07-06 11:51:32 +0200

    [+] JUnit Jupiter

---
+++ Spring-21-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add(){
+        int actual = Calculator.add(2,3);
+    }
+    @Test
+    void TestCase1(){
+
+    }
+    @Test
+    void TestCase2(){
+
+    }
+
+    @Test
+    void TestCase3(){
+
+    }
+
+    @Test
+    void TestCase4(){
+
+    }
+
+    @Test
+    void TestCase5(){
+
+    }
+
+}
